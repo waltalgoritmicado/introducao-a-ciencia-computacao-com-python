d0e2c930d8a84f97ae7286d255ad95d91361cc3b
Walt <waltf4code@gmail.com>
2024-08-07 09:28:29 -0300

    valores e tipos

---
+++ valores_e_tipos.py
@@ -0,0 +1,27 @@
+type(10)
+type('tudo bem')
+
+type(5 / 2)
+
+print(10 / 3);
+print(10 // 3);
+
+print(10 % 3)
+
+peso = 78
+altura = 1.83
+
+
+type(altura)
+
+IMC = peso / (altura ** 2)
+
+IMCInteniro = int(IMC)
+
+print('IMC inteiro: 'IMCInteiro)
+
+
+texto = 'Bom dia, tudo bem?'
+print(len(texto))
+
+
