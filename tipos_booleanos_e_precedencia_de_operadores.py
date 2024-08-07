76d78f57a65b7dd045456681f3ae65834a2cec67
Walt <waltf4code@gmail.com>
2024-08-07 12:10:08 -0300

    tipos booleanos e precedencia de operadores

---
+++ tipos_booleanos_e_precedencia_de_operadores.py
@@ -0,0 +1,19 @@
+print(5 > 3)
+
+x = 12312
+
+print(x < 0 and x == 1000)
+
+print(not True)
+print(not False)
+
+print(not not False)
+print(not not True)
+
+
+chamarElaParaSair = True
+elaAceitar = False
+
+beijarEla = chamarElaParaSair and elaAceitar
+print('Vou beijar ela ', beijarEla)
+
