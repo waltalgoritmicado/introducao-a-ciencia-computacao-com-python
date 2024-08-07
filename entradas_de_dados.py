ef0c1dc3d76935f994d30e83bf1b586353dfcd00
Walt <waltf4code@gmail.com>
2024-08-07 11:43:56 -0300

    entradas de dados

---
+++ entradas_de_dados.py
@@ -0,0 +1,3 @@
+temperaturaFahrenheit = float(input('Digite uma temperatura em Fahrenheit '))
+temperaturaCelsius = (temperaturaFahrenheit - 32) * 5 / 9
+print('A temperatura Celsius eh ', temperaturaCelsius)
