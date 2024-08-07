cf9a14f87813f4171274ed51a3757996ad96549d
Walt <waltf4code@gmail.com>
2024-08-07 08:53:06 -0300

    variaveis e scripts

---
+++ variaveis_e_scripts.py
@@ -0,0 +1,21 @@
+y = 130
+
+x = 5
+x = 345
+x = x + 10
+print(x)
+
+soma = x + y
+
+frase = 'A massa ainda comerá do fino do fino biscoito que fabrico'
+
+peso = 78
+altura = 183
+
+
+a = 10
+b = 20
+
+soma2 = a + b
+print('A soma dos números eh: ', soma2)
+
