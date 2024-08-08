9c4c22d62e6974d6f0ed5ddf6a0ff7588c438513
Walt <waltf4code@gmail.com>
2024-08-08 19:38:45 -0300

    funções

---
+++ funcoes.py
@@ -0,0 +1,17 @@
+def soma(x, y):
+    return x + y;
+
+def multiplica(x, y, z):
+    return x * y * z;
+
+def nome_do_seu_time():
+    return 'Flamengo'   
+
+def bom_dia():
+    print('Bom dia Andrielli')
+
+print('Soma ', soma(10, 20))
+print('Multiplicão ', multiplica(10, 20, 30))
+print(nome_do_seu_time())
+bom_dia()
+
